bc30d3cd54493b27e5750e76bbfcab68ad45e188
Rina Trofimova <lidesrose@gmail.com>
2021-02-24 22:12:44 +0700

    Done

---
+++ Flight/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FlightBack;
+using System.Windows.Forms;
+
+namespace Flight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Backend newBack = new Backend();
+
+        int tick;
+        double t;
+        Point Coordinates;
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            newBack.GivingFunc((double)edAngle.Value, (double)edSpeed.Value, (double)edHeight.Value, (double)edWeight.Value, (double)edSquare.Value);
+            
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, (double)edHeight.Value);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t = newBack.IncreaseT(t);
+            tick = newBack.IncreaseTick(tick);
+            label7.Text = tick.ToString();
+            label8.Text = t.ToString();
+            Coordinates = newBack.NextStep();
+
+            chart1.Series[0].Points.AddXY(Coordinates.x, Coordinates.y);
+            if (Coordinates.y <= 0) timer1.Stop();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
---
+++ FlightBack/Backend.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBack
+{
+    public class Backend
+    {
+        const double dt = 0.1;
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+        double t = 0;
+        int tick = 0;
+        double k, vx, vy, x, y;
+        Point ans = new Point();
+
+        static void Main(string[] args)
+        {
+        }
+        public void GivingFunc(double a, double v0, double y0, double m, double S)
+        {
+            k = 0.5 * C * S * rho / m;
+            vx = Speed(v0, func.cos, a);
+            vy = Speed(v0, func.sin, a);
+            ans.x = 0;
+            ans.y = y0;
+        }
+
+        public Point NextStep()
+        {
+            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
+            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
+
+            ans.x = ans.x + vx * dt;
+            ans.y = ans.y + vy * dt;
+            return ans;
+        }
+        
+        public double IncreaseT(double t)
+        {
+            t += dt;
+            return t;
+        }
+        public int IncreaseTick(int tick)
+        {
+            tick++;
+            return tick;
+        }
+        
+        private double DegToRad(double a)
+        {
+            return a * Math.PI / 180;
+        }
+
+        private double Speed(double v0, func f, double a)
+        {
+            if (f == func.cos)
+            {
+                return v0 * Math.Cos(DegToRad(a));
+            }
+            else
+            {
+                return v0 * Math.Sin(DegToRad(a));
+            }
+        }
+
+    }
+
+    public enum func
+    {
+        cos,
+        sin
+    }
+}
